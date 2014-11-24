b5d730e55939bea21c2190a2de8c2705d8941ec2
Daniel Naab <danielnaab@gmail.com>
2014-11-23 19:06:58 -0600

    Add a way to bootstrap page-specific Javascript.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -3,9 +3,25 @@
 var $ = require('zepto-browserify').$
 
 
+// Each page should export a function to be called on page ready.
+var pages = {
+    testimonials: require('./pages/testimonials')
+}
+
+
+// Fade out header image as the page scrolls down.
 $(window).on('scroll', function() {
     var opac = 1 - $(window).scrollTop() / $('header').height()
     $('header').css({
         opacity: opac
     })
+})
+
+
+// Execute page-specific Javascript.
+$(document).ready(function() {
+    var page = $('body').prop('className')
+    if (pages.hasOwnProperty(page)) {
+        pages[page]()
+    }
 })
