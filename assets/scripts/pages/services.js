5efab0a9e63f5c3bb45769211da5191af1862dab
Daniel Naab <danielnaab@gmail.com>
2015-09-15 12:54:43 -0500

    Update services pages so default link doesn't scroll down.

--- assets/scripts/pages/services.js
+++ assets/scripts/pages/services.js
@@ -7,6 +7,10 @@ module.exports = function() {
     var scrollTop = function () {
         window.scrollTo(0, 0)
     }
-    $(window).ready(scrollTop)
+    $(window).ready(function () {
+        if (!window.location.hash) {
+            window.location.hash = $('article').first().attr('id')
+        }
+    })
     $(window).on('hashchange', scrollTop)
 }
